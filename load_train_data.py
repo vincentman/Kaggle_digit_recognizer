317b3675a1e82c9af28de65d851d0196c2314a5a
vincentkao <vincentkao@ibms.sinica.edu.tw>
2018-04-26 17:36:05 +0800

    svm

--- load_train_data.py
+++ load_train_data.py
@@ -5,31 +5,7 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import math
 
-def get_data_array(is_train, file_path):
-    xlist, ylist = [], []
-    if is_train:
-        data_ratio = 0.7 if is_train else 0.3
-    with open(file_path, 'r') as file:
-        csv_reader = csv.reader(file, delimiter=',')
-        next(csv_reader)  # skip header
-        count = 0
-        for line in csv_reader:
-            if count >= 42000*data_ratio:
-                break
-            xlist.append(line[1:785])
-            ylist.append(line[0])
-            count += 1
-    x_train = np.asarray(xlist).astype('float32')
-    sc = StandardScaler()
-    sc.fit(x_train)
-    x_train = sc.transform(x_train)
-    y_train = to_categorical(np.asarray(ylist, dtype=np.float32), 10)
-
-    print(x_train.shape)
-    print(y_train.shape)
-    return x_train, y_train
-
-def get_pandas_data(is_train, file_path):
+def get_data(is_train, file_path):
     if is_train:
         data_ratio = 0.7 if is_train else 0.3
     pd_csv = pd.read_csv(file_path)
--- svm_train.py
+++ svm_train.py
@@ -1,10 +1,8 @@
-# from load_train_data import get_data_array
-from load_train_data import get_pandas_data
+from load_train_data import get_data
 
-x_train, y_train = get_pandas_data(True, 'train.csv')
-# x_train, y_train = get_data_array(True, 'train.csv')
+x_train, y_train = get_data(True, 'train.csv')
 
 from sklearn.svm import SVC
-svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0, verbose=True)
 svm.fit(x_train, y_train)
 print(svm.score(x_train, y_train))
